b550097de74b343e9bc3da5ccf5b0789a2b3ac55
Santiago Aramburu <santi.aramburu45@gmail.com>
2025-08-20 09:54:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// Datos simulados 
+const empleados = [
+  {
+    FirstName: "Tanmay",
+    LastName: "Patil",
+    Address: { City: "Bangalore" }
+  },
+  {
+    FirstName: "Ramona",
+    LastName: "Perez",
+    Address: { City: "Montevideo" }
+  },
+  {
+    FirstName: "Sofia",
+    LastName: "Wetther",
+    Address: { City: "Canelones" }
+  },
+  {
+    FirstName: "Olivia",
+    LastName: "Rodrigo",
+    Address: { City: "San José" }
+  },
+  {
+    FirstName: "Taylor",
+    LastName: "Swift",
+    Address: { City: "Pando" }
+  }
+];
+
